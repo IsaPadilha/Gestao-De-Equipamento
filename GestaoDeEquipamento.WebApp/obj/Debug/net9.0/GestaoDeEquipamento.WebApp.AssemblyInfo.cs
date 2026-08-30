8e387104e93c46b618a96836001475d3d374997e
Isadora Padilha <isap4619@gmail.com>
2026-08-30 17:26:18 -0300

    adicionado chamado ao _ViewImports

--- GestaoDeEquipamento.WebApp/obj/Debug/net9.0/GestaoDeEquipamento.WebApp.AssemblyInfo.cs
+++ GestaoDeEquipamento.WebApp/obj/Debug/net9.0/GestaoDeEquipamento.WebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestaoDeEquipamento.WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49ac1bfb1171dd04e0a3d2471cd493732e8720be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a776da34ca5664651698d3d801d2dc988a02f74")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestaoDeEquipamento.WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestaoDeEquipamento.WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
